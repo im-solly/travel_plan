da3f125b5138e377d428050e074a811387cb6a06
im-solly <pbmi97@gmail.com>
2024-08-22 09:49:34 +0900

    travel information added

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from PIL import Image
 import streamlit as st
 import openai
 import requests
+import os
 
 # OpenAI API 키 설정
-openai.api_key = 'api-key'
+API_KEY = os.environ['API_KEY']
+openai.api_key = API_KEY
 
 def get_openai_response(prompt):
     """
@@ -24,22 +26,26 @@ def get_openai_response(prompt):
     return response.choices[0].message['content']
 
 def main():
+    print(API_KEY)
     st.title('여행 계획 세우기 귀찮지? 생성 AI에게 물어보자!')
     st.image("travel.jpg", use_column_width=True)
+
     # 사이드바에서 사용자 정보 입력받기
     question_gender = st.selectbox("성별을 입력해주세요", ("남자", "여자"))
     question_age = st.text_input("나이를 입력해 주세요")
     question_place = st.text_input("가고 싶은 여행지를 입력해 주세요")
-#    question_date = st.text_input("원하는 여행 기간(날짜)을 입력해 주세요")
+    question_must = st.text_input("꼭 가보고 싶었던 장소가 있나요?")
+    question_transportation = st.selectbox("어떤 교통수단을 이용하나요?", ("렌트카", "대중교통"))
     question_start_date = st.date_input("여행 시작 날짜를 입력해 주세요")
     question_end_date = st.date_input("여행 종료 날짜를 입력해 주세요")
 
     if st.button("Send"):
         if question_place and question_gender and question_age and question_start_date and question_end_date:
             # 사용자 입력과 사이드바 정보를 프롬프트에 포함
-            prompt = (f"성별: {question_gender}, 나이: {question_age}. "
+            prompt = (f"너는 고객의 정보를 받아 여행 경로를 추천해주는 전문가야. 성별: {question_gender}, 나이: {question_age}. "
                       f"여행지: {question_place}, 여행기간: {question_start_date} ~ {question_end_date}. "
-                      f"이 정보를 바탕으로 여행 스케줄을 추천해줘.")
+                      f"이 정보를 바탕으로 여행 스케줄을 추천해줘. {question_must}는 꼭 방문하고 싶고, 스케줄을 추천해줄 때 이동 시에는 {question_transportation}을 이용할 거야."
+                      f"사용되어야 하는 교통수단, 교통비, 입장료 등을 계산해서 같이 알려줘. ")
             with st.spinner('생성 AI로 답변을 생성하는 중입니다 ...'):
                 response = get_openai_response(prompt)
             st.markdown(f'**Bot:**\n{response}')
